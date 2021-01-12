b0edf06887e32caf7ce155af4e0765b5b157e766
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-01-12 12:21:49 -0500

    Section 4: Part 16-17 Completed

---
+++ config/db.js
@@ -0,0 +1,22 @@
+// ES6 DB Connection Module
+
+import mongoose from 'mongoose'
+
+// Async/Await Arrow Function that receives a promise and must be wrapped in a try/catch
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+export default connectDB
